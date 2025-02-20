5bc0b4ef913099aa0ef239682aa09c33a9d6abd5
jean-cheramy <cheramy.jean.emile@gmail.com>
2025-02-20 14:45:09 +0100

    add load and write s3 bucket for sitemap urls extraction

--- lambda/urls-check/main.py
+++ lambda/urls-check/main.py
@@ -1,19 +1,51 @@
-import requests
+import json
+import os
 import re
+
+import boto3
+import requests
 from bs4 import BeautifulSoup
 
+s3_client = boto3.client("s3")
+bucket_name = os.environ["BUCKET_NAME"]
 
-import json
 
+def load_from_s3(file_name):
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        file_content = response["Body"].read().decode("utf-8")
+        return json.loads(file_content)
+    except Exception as e:
+        print(e)
+        return []
+
+def save_into_s3(file_name, json_data):
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=json_data,
+        ContentType="application/json"
+    )
 
 def handler(event, context):
-    url = "https://www.rtbf.be/site-map/articles5000.xml"
-    response = requests.get(url, timeout=10)
+    processed_file_name = os.environ["FILE_NAME"]
+    rtbf_url = os.environ["RTBF_URL"]
+
+    response = requests.get(rtbf_url, timeout=10)
     image_regex = re.compile(r".*[a-zA-Z]$", re.IGNORECASE)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "xml")
-        urls = [loc.text.strip() for loc in soup.find_all("loc") if not image_regex.match(loc.text.strip())]
-        return {"statuscode": 200, "body": json.dumps({"urls": urls})}
-    else:
-        print(f"Failed to fetch XML: {response.status_code}")
-        return None
+        to_process_filename = "to_process.json"
+        return extract_urls(
+            response, image_regex, processed_file_name, to_process_filename
+        )
+    print(f"Failed to fetch XML: {response.status_code}")
+    return None
+
+
+def extract_urls(response, image_regex, processed_file_name, to_process_filename):
+    soup = BeautifulSoup(response.content, "xml")
+    urls = [loc.text.strip() for loc in soup.find_all("loc") if not image_regex.match(loc.text.strip())]
+    processed_url = load_from_s3(processed_file_name)
+    to_process_url = set(urls).difference(set(processed_url))
+    save_into_s3(to_process_filename, json.dumps(to_process_url))
+    return {"statuscode": 200, "#urls": len(to_process_url),"urls": json.dumps({"urls": to_process_url})}
--- rtbf_news_scraper_aws/rtbf_news_scraper_aws_stack.py
+++ rtbf_news_scraper_aws/rtbf_news_scraper_aws_stack.py
@@ -3,7 +3,12 @@ import os
 import aws_cdk as cdk
 from aws_cdk import (
     Stack)
-from aws_cdk import aws_lambda as _lambda
+from aws_cdk import (
+    core as cdk,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_iam as iam
+)
 from constructs import Construct
 
 
@@ -13,11 +18,33 @@ class RtbfNewsScraperAwsStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
 
+        # Create an S3 bucket
+        urls_bucket = s3.Bucket(self, "UrlsBucket",
+                           removal_policy=cdk.RemovalPolicy.DESTROY)
+
+        # IAM Role for Lambda to access S3
+        urls_lambda_role = iam.Role(self, "LambdaS3Role",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
+            ]
+        )
+
         urls_lambda = _lambda.DockerImageFunction(
             self, "GetUrlsSitemap",
             memory_size=2048,
             code=_lambda.DockerImageCode.from_image_asset(
                 directory=os.path.join(os.path.dirname(__file__), "../lambda/urls-check")),
-            timeout=cdk.Duration.minutes(15)
+            timeout=cdk.Duration.minutes(15),
+            environment={
+                "BUCKET_NAME": urls_bucket.bucket_name,
+                "FILE_NAME": "processed_urls.json",
+                "RTBF_URL" : "https://www.rtbf.be/site-map/articles5000.xml"
+            },
+            role=urls_lambda_role
         )
         cdk.CfnOutput(self, "LambdaFunctionArn", value=urls_lambda.function_arn)
+
+        #urls_bucket.grant_write(urls_lambda)
+        #urls_bucket.grant_read(urls_lambda)
